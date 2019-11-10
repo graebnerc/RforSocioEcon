92d31bf6180e327a750b0af379b97ee33fadb4e0
Claudius Graebner <claudius@claudius-graebner.com>
2019-11-10 13:08:22 +0100

    First draft of data chapter

--- R/knit-chapter.R
+++ R/knit-chapter.R
@@ -3,5 +3,7 @@ library(here)
 stat_chap <- here("ChapA-WdhlStatistik1.Rmd")
 markdown_chap <- here("ChapA-Markdown.Rmd")
 prev_chapter <- here("ChapA-WdhlStatistik1.Rmd")
+vis_chap <- here("Chap-visualization.Rmd")
+data_chap <- here("Chap-data.Rmd")
 
-preview_chapter(input=markdown_chap, output_dir=here("docs"))
+preview_chapter(input=data_chap, output_dir=here("docs"))
