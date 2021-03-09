f04e2182f504409e318208bf3726b897e19107b7
Claudius Graebner <claudius@claudius-graebner.com>
2021-03-09 15:31:28 +0100

    fix save_pdf_png function

--- R/helpers.R
+++ R/helpers.R
@@ -1,18 +1,15 @@
 #' Saves a plot both as pdf and png
 save_pdf_png <- function(filename, plot = last_plot(), ...){
-  if (substr(file_name, nchar(file_name)-3, nchar(file_name)) == ".pdf"){
+  if (substr(filename, nchar(filename)-3, nchar(filename)) == ".pdf"){
     filename_pdf <- filename
-    filename_png <- gsub(".pdf", ".png", file_name)
-  } else if (substr(file_name, nchar(file_name)-3, nchar(file_name)) == ".png"){
+    filename_png <- gsub(".pdf", ".png", filename)
+  } else if (substr(filename, nchar(filename)-3, nchar(filename)) == ".png"){
     filename_png <- filename
-    filename_pdf <- gsub(".png", ".pdf", file_name)
+    filename_pdf <- gsub(".png", ".pdf", filename)
   } else{
     filename_pdf <- paste0(filename, ".pdf")
-    filename_pdf <- paste0(filename, ".png")
+    filename_png <- paste0(filename, ".png")
   }
-
   ggsave(filename = filename_pdf, plot = plot, ...)
   ggsave(filename = filename_png, plot = plot, ...)
   }
-
-save_pdf_png(filename = file_name, height=6, width=8)
