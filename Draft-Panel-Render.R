60c27a15b0d7303f6d3cba81260460d4dd41de31
Claudius Graebner <claudius@claudius-graebner.com>
2021-03-14 10:12:47 +0100

    Continue with panel draft

---
+++ Draft-Panel-Render.R
@@ -0,0 +1,7 @@
+
+rmarkdown::render(
+  "Draft-Paneloekonometrie-1.Rmd", output_format = "pdf_document",
+  output_dir = here::here("Drafts"))
+rmarkdown::render(
+  "Draft-Paneloekonometrie-1.Rmd", output_format = "html_document",
+  output_dir = here::here("Drafts"))
